39ea4aa5671ef783db7cad4fd579a3d80fe7f5a9
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-02-01 14:04:50 +0100

    removed noise

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App Content</h1>
+    <div className="container">
+      <form>
+        <h1>Registration Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui-form"></div>
+
+        {/* Username */}
+        <div className="field">
+          <label>Username: </label>
+          <input type="text" name="username" placeholder="username" />
+        </div>
+        {/* Email */}
+        <div className="field">
+          <label>Email: </label>
+          <input type="email" name="email" placeholder="Email" />
+        </div>
+        {/* Password */}
+        <div className="field">
+          <label>Password: </label>
+          <input type="password" name="password" placeholder="password" />
+        </div>
+      </form>
     </div>
   );
 }
